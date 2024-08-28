9d5915c451838452382b25977bdb0bc7b4584e57
RobdeMooij <45096223+RobdeMooij@users.noreply.github.com>
2024-08-28 09:54:59 +0200

    Add basic comments

--- src/qcardia_data/pipeline/transforms/dimensions.py
+++ src/qcardia_data/pipeline/transforms/dimensions.py
@@ -17,15 +17,20 @@ class SplitDimensionsd(MapTransform, MultiSampleTrait):
         super().__init__(keys, allow_missing_keys)
         self.ignore_selected_frames = ignore_selected_frames
 
-    def split_frames(self, data):
+    def split_frames(self, data: dict) -> list[dict]:
         d = deepcopy(data)
+
+        # if only one frame is present, return the data without the frame dimension
         total_nr_frames = d[self.first_valid_key(d)].shape[4]
         if total_nr_frames == 1:
             for key in self.key_iterator(d):
                 d[key] = data[key][:, :, :, :, 0].clone()
             return [d]
+
+        # if multiple frames are present, add total_nr_frames to meta_dict and continue
         d["meta_dict"]["total_nr_frames"] = total_nr_frames
 
+        # select frames to include and remove selected_frame_nrs from meta_dict
         frame_nrs = d["meta_dict"].pop("selected_frame_nrs")
         if self.ignore_selected_frames:
             frame_nrs = torch.arange(total_nr_frames)
@@ -35,19 +40,23 @@ class SplitDimensionsd(MapTransform, MultiSampleTrait):
             d["meta_dict"]["frame_nr"] = frame_nr.item()
             for key in self.key_iterator(d):
                 if self.ignore_selected_frames or data[key].shape[4] != len(frame_nrs):
-                    d[key] = data[key][:, :, :, :, frame_nr].clone()
+                    d[key] = data[key][:, :, :, :, frame_nr].clone()  # selected frames
                 else:
-                    d[key] = data[key][:, :, :, :, idx].clone()
+                    d[key] = data[key][:, :, :, :, idx].clone()  # all time frames
             frames.append(deepcopy(d))
         return frames
 
-    def split_slices(self, data):
+    def split_slices(self, data: dict) -> list[dict]:
         d = deepcopy(data)
+
+        # if only one slice is present, return the data without the slice dimension
         total_nr_slices = d[self.first_valid_key(d)].shape[3]
         if total_nr_slices == 1:
             for key in self.key_iterator(d):
                 d[key] = data[key][:, :, :, 0, ...].clone()
             return [d]
+
+        # if multiple slices are present, add total_nr_slices to meta_dict and continue
         d["meta_dict"]["total_nr_slices"] = total_nr_slices
 
         slices = []
@@ -58,7 +67,7 @@ class SplitDimensionsd(MapTransform, MultiSampleTrait):
             slices.append(deepcopy(d))
         return slices
 
-    def first_valid_key(self, d):
+    def first_valid_key(self, d: dict):
         for key in self.key_iterator(d):
             if key is not None:
                 return key
@@ -73,7 +82,7 @@ class DimensionsTo2Dd(SplitDimensionsd):
         keys: KeysCollection,
         allow_missing_keys: bool = False,
         ignore_selected_frames: bool = False,
-    ) -> None:
+    ):
         super().__init__(keys, allow_missing_keys, ignore_selected_frames)
 
     def __call__(self, data):
@@ -92,7 +101,7 @@ class DimensionsTo3Dd(SplitDimensionsd):
         keys: KeysCollection,
         allow_missing_keys: bool = False,
         ignore_selected_frames: bool = False,
-    ) -> None:
+    ):
         super().__init__(keys, allow_missing_keys, ignore_selected_frames)
 
     def __call__(self, data):
