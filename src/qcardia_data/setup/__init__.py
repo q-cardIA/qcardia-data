3a2f1c1a6fe61a5d52b196be7d91775099f7f382
Cian Scannell <cianmscannell@gmail.com>
2024-09-07 01:06:25 +0200

    removed reference to setup LGE

--- src/qcardia_data/setup/__init__.py
+++ src/qcardia_data/setup/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["setup_cine", "setup_lge", "explore_development_data_from_config"]
+__all__ = ["setup_cine", "explore_development_data_from_config"]
 
-from qcardia_data.setup.data_setup import setup_cine, setup_lge
+from qcardia_data.setup.data_setup import setup_cine
 from qcardia_data.setup.exploration import explore_development_data_from_config
